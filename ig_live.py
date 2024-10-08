92fd160781cd9751e1dbcf2393f09b7715018f09
Varun <valivetiavrun@gmail.com>
2024-10-07 23:38:28 -0700

    Basic facial recog complete from video (in future will enhance frames as needed); next step to process images via online tools

--- ig_live.py
+++ ig_live.py
@@ -1,22 +1,52 @@
 import cv2 as cv
+from mtcnn.mtcnn import MTCNN
+import os 
 
+print("hi")
+detector = MTCNN()
 
+#given video (later will be changed to live stream) get the faces
 
-capture = cv.VideoCapture(0)
+imagePathArr = []
+def extractFaces(videoPath, output, interval=5, padding=40):
 
-if not capture:
-    exit()
+    if not os.path.exists(output):
+        os.makedirs(output)
+    
+    capture = cv.VideoCapture(videoPath)
+    frameCount = 0
+    faces = 0
+    
+    while True:
 
-while True:
-    ret, frame = capture.read()
+        ret, frame = capture.read()
+        
+        if not ret:
+            break
+        
+        if frameCount % interval == 0:
+            results = detector.detect_faces(frame)
+        
+            for result in results:
+                x,y,w,h = result["box"]
+                keypoints = result["keypoints"]
 
-    if not ret:
-        break
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    cv.imshow('frame', gray)
-    if cv.waitKey(1) == ord('f'):
-        break
+                x1 = max(0,x-padding)
+                y1 = max(0,y-padding)
+                x2 = min(frame.shape[1], x+w+padding)
+                y2 = min(frame.shape[0],y+h+padding)
+                face = frame[y1:y2, x1:x2]
 
-capture.release()
-capture.destroyAllWindows()
+                faceFileName = os.path.join(output, f"face_{faces}.jpg")
+                cv.imwrite(faceFileName, face)
+                faces+=1
+        
+        frameCount+=1
 
+    capture.release()
+
+    
+videoPath = "test.mp4"
+outputPath = "/Users/varunvaliveti/Desktop/CS Files/Personal /M_Recognition/green"
+extractFaces(videoPath, outputPath, interval=5)
+print("done")
